48dd2e271ba671f604a0a80a55ead0579bf7a1da
Dumisani <dumisanimbanga21@gmail.com>
2026-06-09 16:33:02 +0200

    Complete reset to stable state + HTTPS fixes

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('x-forwarded-proto') === 'https' || env('FORCE_HTTPS')) {
+            $request->server->set('HTTPS', 'on');
+            \URL::forceScheme('https');
+        }
+        return $next($request);
+    }
+}
